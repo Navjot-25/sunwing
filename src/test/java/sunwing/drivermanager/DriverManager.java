99e71ca4de4f42aa5ec0d513ffebf7492d14b1e9
navjot kaur <khasariaharmansingh@gmail.com>
2020-04-03 20:10:10 -0400

    drivermanager class and hometest class is updated

--- src/test/java/sunwing/drivermanager/DriverManager.java
+++ src/test/java/sunwing/drivermanager/DriverManager.java
@@ -1,5 +1,58 @@
 package sunwing.drivermanager;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverManager {
+	public static WebDriver driver;
+	public static FileInputStream fis;
+	public static Properties prop;
+	public static void initDriver()
+	{
+		try {
+			fis=new FileInputStream("C:\\Users\\khasaria\\Downloads\\gitpracticewithnavjot\\sunwing\\src\\test\\java\\sunwing\\resources\\browser.properties");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		prop = new Properties();
+		try {
+			prop.load(fis);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome") )
+		{
+			 WebDriverManager.chromedriver().setup();
+			 driver=new ChromeDriver();
+	    }
+		else if(prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+			
+		}
+		else {
+			 WebDriverManager.chromedriver().setup();
+			 driver=new ChromeDriver();
+		}
+	}
+	public static WebDriver getDriverInstance()
+	{
+		if(driver==null) {
+			initDriver();
+		}
+		return driver;
+	}
+	
 
 }
--- src/test/java/sunwing/testcases/HomeTest.java
+++ src/test/java/sunwing/testcases/HomeTest.java
@@ -1,5 +1,29 @@
 package sunwing.testcases;
 
-public class HomeTest {
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
+import sunwing.drivermanager.DriverManager;
+
+public class HomeTest 
+{
+	WebDriver driver=DriverManager.getDriverInstance();
+    @BeforeSuite
+    public void setup()
+    {
+    	driver.get("https://www.sunwing.ca/");
+    }
+    @Test
+    public void getTitle()
+    {
+    	driver.getTitle();
+    	System.out.println(driver.getTitle());
+    }
+    @AfterSuite
+    public void terminate()
+    {
+    	driver.quit();
+    }
 }
